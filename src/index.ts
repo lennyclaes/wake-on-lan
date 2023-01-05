4816aab4afb4e5512557080d3228e7afebbcafd0
Lenny Claes <lenny.claes@gmail.com>
2023-01-05 02:29:29 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,74 @@
+const dgram = require('node:dgram');
+import { Buffer } from 'node:buffer';
+import { Socket } from 'node:dgram';
+
+/**
+ * Creates the magic packet based on the MAC address and returns it as a buffer
+ * @param mac The MAC address from which the magic packet is formed
+ * @returns magicPacketBuffer
+ */
+function createMagicPacket(mac: string): Buffer {
+    const macRepeats: number = 16;
+    const macBufferSize: number = 6;
+    const packetHeader: number = 6;
+
+    // Create the buffer and fill first 6 bytes with FF
+    let magicPacketBuffer = Buffer.alloc(packetHeader);
+    magicPacketBuffer.fill(0xff);
+
+    // Split the MAC address in segments, convert them to a buffer and concate this buffer 16 times with the magicPacketBuffer
+    const segments: RegExpMatchArray | null = mac.match(/[0-9a-fA-F]{2}/g);
+    if (!segments || segments.length != macBufferSize) {
+        throw new Error(`invalid MAC-address ${mac}`);
+    }
+    const parsedSegments: number[] = segments.map((seg: string) => parseInt(seg, 16));
+    const bufferedMac: Buffer = Buffer.from(parsedSegments);
+    for (let i = 0; i < macRepeats; i++) {
+        magicPacketBuffer = Buffer.concat([magicPacketBuffer, bufferedMac]);
+    }
+
+    return magicPacketBuffer;
+}
+
+/**
+ * Broadcasts the Wake-On-Lan message to the specified MAC address
+ * @param mac The MAC address for which the broadcast is meant.
+ * @param retries The amount of times the broadcast should be sent. Defaults to 1
+ * @returns A promise that returns a string with a success message
+ */
+function sendWake(mac: string, retries: number = 1): Promise<string> {
+    console.log(retries);
+    // Later version: add support for ipv6
+    const socket: Socket = dgram.createSocket('udp4');
+    return new Promise((resolve, reject) => {
+        let magicPacket: Buffer;
+        try {
+            magicPacket = createMagicPacket(mac);
+        } catch (err: any) {
+            return reject(new Error(`Failed to broadcast wake: ${err.message}`));
+        }
+        socket
+            .on('error', (err: Error) => {
+                reject(err.stack);
+            })
+            .once('listening', () => {
+                socket.setBroadcast(true);
+            });
+        let attempts = 1;
+        const sendInterval: NodeJS.Timer = setInterval(() => {
+            socket.send(magicPacket, 0, magicPacket.length, 9, '192.168.0.255', (err: Error | null) => {
+                if (err) {
+                    return reject(new Error(`Failed to broadcast wake: ${err.stack}`));
+                }
+                if (attempts >= retries) {
+                    resolve(`Broadcast(s) succesfully sent to ${mac}`);
+                    socket.close();
+                    clearInterval(sendInterval);
+                }
+                attempts++;
+            });
+        }, 350);
+    })
+}
+
+export default sendWake;
